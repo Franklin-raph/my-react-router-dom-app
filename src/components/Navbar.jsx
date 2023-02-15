96942cebaa40bcb2c04f81993a4f74bee8af9e61
franklin-raph <igboekwulusifranklin@gmail.com>
2023-02-15 06:13:16 +0100

    made an animated nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false)
+
+  const handleNav = () => {
+    setIsNavOpen(!isNavOpen)
+  }
+
   return (
-    <div className='nav' style={{  }}>
+    <nav>
+      <h3>Nav</h3>
+      <i onClick={handleNav}>open</i>
+      <div className={isNavOpen ? "showNav" : ""}>
+        <i onClick={handleNav}>close</i>
         <Link to="/">Home</Link>
         <Link to="/contact">Contact</Link>
         <Link to="/about">About</Link>
-    </div>
+      </div>
+    </nav>
   )
 }
 
